e3b145c23fd0b23b5e6dc52cf07496bab23933a2
DocG1956 <garybales1029@yahoo.com>
2020-09-19 18:03:05 -0500

    fixed node update that failed that was causing app to fail, Going to start branching from now on in case something breaks and I can't determine the cause,

---
+++ ClientApp/src/app/cockpit/cockpit.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { CockpitComponent } from './cockpit.component';
+
+let component: CockpitComponent;
+let fixture: ComponentFixture<CockpitComponent>;
+
+describe('cockpit component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ CockpitComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(CockpitComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ ClientApp/src/app/server-element/server-element.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ServerElementComponent } from './server-element.component';
+
+let component: ServerElementComponent;
+let fixture: ComponentFixture<ServerElementComponent>;
+
+describe('Server-element component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ServerElementComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(ServerElementComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
